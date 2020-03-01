dded9091d2dc2932b12e1f74d081bc2db4898fce
kiwipeace01 <60574300+kiwipeace01@users.noreply.github.com>
2020-03-02 00:21:27 +0530

    Update sketch_mar01a.ino

--- sketch_mar01a/sketch_mar01a.ino
+++ sketch_mar01a/sketch_mar01a.ino
@@ -2,28 +2,54 @@ int motor1_IN1=11;
 int motor1_IN2=12;
 int motor2_IN3=9;
 int motor2_IN4=10;
+const int trigPin_0= 2;
+const trigPin_90=8;
+const echoPin_90=7;
+const int echoPin_0 = 5;
+
+long duration_0,duration_90;
+int distance_0,distance_90;
 
 char state;
-void loop(){
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(200);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(200);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration * .0343) / 2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
-}
+
 void setup() 
 {
   pinMode(motor1_IN1,OUTPUT);
   pinMode(motor1_IN2,OUTPUT);
   pinMode(motor2_IN3,OUTPUT);
   pinMode(motor2_IN4,OUTPUT);
+  pinMode(trigPin_0, OUTPUT); 
+  pinMode(echoPin_0, INPUT); 
+  pinMode(trigPin_90, OUTPUT);
+  pinMode(echoPin_90, INPUT);
   Serial.begin(9600);
-
 }
+
+float read_sensor1()
+{
+  digitalWrite(trigPin_0, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin_0, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin_0, LOW);
+  duration_0 = pulseIn(echoPin_0, HIGH);
+  distance_0= duration_0*0.034/2;
+  return distance_0;
+}
+
+float read_sensor2()
+{
+// second ultrasonic sensor
+  digitalWrite(trigPin_90, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin_90, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin_90, LOW);
+  duration_90 = pulseIn(echoPin_90, HIGH);
+  distance_90= duration_90*0.034/2;
+  return distance_90;
+}
+
 
 void loop()
 {
