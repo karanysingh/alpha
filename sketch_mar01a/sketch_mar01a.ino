2e843a262dbcdab2619ab0d569006c2927a378d2
Shivratna <60550869+Shivratna@users.noreply.github.com>
2020-03-01 17:04:27 +0530

    Add files via upload

---
+++ sketch_mar01a/sketch_mar01a.ino
@@ -0,0 +1,79 @@
+int motor1_IN1=11;
+int motor1_IN2=12;
+int motor2_IN3=9;
+int motor2_IN4=10;
+
+char state;
+void setup() 
+{
+  pinMode(motor1_IN1,OUTPUT);
+  pinMode(motor1_IN2,OUTPUT);
+  pinMode(motor2_IN3,OUTPUT);
+  pinMode(motor2_IN4,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void Left()
+{
+  digitalWrite(motor1_IN1,LOW);
+  digitalWrite(motor1_IN2,HIGH);
+  digitalWrite(motor2_IN3,HIGH);
+  digitalWrite(motor2_IN4,LOW);
+}
+void Right()
+{
+  digitalWrite(motor1_IN1,HIGH);
+  digitalWrite(motor1_IN2,LOW);
+  digitalWrite(motor2_IN3,LOW);
+  digitalWrite(motor2_IN4,HIGH);
+}
+void Front()
+{
+  digitalWrite(motor1_IN1,HIGH);
+  digitalWrite(motor1_IN2,LOW);
+  digitalWrite(motor2_IN3,HIGH);
+  digitalWrite(motor2_IN4,LOW);
+}
+void Back()
+{
+  digitalWrite(motor1_IN1,LOW);
+  digitalWrite(motor1_IN2,HIGH);
+  digitalWrite(motor2_IN3,LOW);
+  digitalWrite(motor2_IN4,HIGH);
+}
+
+void loop()
+{
+ while(Serial.available())
+  {state=Serial.read();}
+
+  switch(state)
+  {
+    case 'F': {
+  digitalWrite(motor1_IN1,HIGH);
+  digitalWrite(motor1_IN2,LOW);
+  digitalWrite(motor2_IN3,HIGH);
+  digitalWrite(motor2_IN4,LOW);
+}
+    case 'L': { digitalWrite(motor1_IN1,HIGH);
+  digitalWrite(motor1_IN2,LOW);
+  digitalWrite(motor2_IN3,LOW);
+  digitalWrite(motor2_IN4,HIGH);
+    }
+    case 'R': {
+  digitalWrite(motor1_IN1,HIGH);
+  digitalWrite(motor1_IN2,LOW);
+  digitalWrite(motor2_IN3,LOW);
+  digitalWrite(motor2_IN4,HIGH);
+}
+    case 'B': {
+  digitalWrite(motor1_IN1,LOW);
+  digitalWrite(motor1_IN2,HIGH);
+  digitalWrite(motor2_IN3,LOW);
+  digitalWrite(motor2_IN4,HIGH);
+}
+           
+  }
+}
+
